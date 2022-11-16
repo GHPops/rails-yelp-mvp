c2cefbf54e2702bfa3c6dacf0a11c885eac9d03d
pauline gouttiere <paulinegouttiere@gmail.com>
2022-11-16 16:12:27 +0100

    exercice le plus long du monde

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'belgian' }
+pizza_east =  { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' }
+pizza_north = { name: 'Pizza North', address: '57A Shoreditch High St, London E1 6PQ', category: 'italian' }
+chinois = { name: 'Bon appetit', address: '36 rue du nem, Paris 11', category: 'chinese' }
+mcdo = { name: 'Mcdo', address: '534 rue du hamburger, Paris 11', category: 'french' }
+
+[dishoom, pizza_east, pizza_north, chinois, mcdo].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
